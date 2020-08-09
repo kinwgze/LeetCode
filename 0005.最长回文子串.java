ba865d9c16b0e112fc9cc92e53487f8b11cc1e38
kinwgze <wz297443228@outlook.com>
2020-08-09 15:38:58 +0800

    expandAroundCenter

---
+++ 0005.最长回文子串.java
@@ -0,0 +1,42 @@
+//暴力破解法无法满足时间复杂度，过不了AC，
+
+//方法一：中心扩展法
+class Solution {
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() == 0) return "";
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i ++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end +1);
+    }
+
+    public int expandAroundCenter(String s, int left, int right) {
+        int L = left, R = right;
+        while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+            L --;
+            R ++;
+        }
+        return R - L - 1;
+    }
+}
+
+/*
+边界情况即为子串长度为 11 或 22 的情况。我们枚举每一种边界情况，并从对应的子串开始不断地向两边扩展。
+如果两边的字母相同，我们就可以继续扩展，例如从 P(i+1,j-1)P(i+1,j−1) 扩展到 P(i,j)P(i,j)；
+如果两边的字母不同，我们就可以停止扩展，因为在这之后的子串都不能是回文串了。
+
+聪明的读者此时应该可以发现，「边界情况」对应的子串实际上就是我们「扩展」出的回文串的「回文中心」。
+方法的本质即为：我们枚举所有的「回文中心」并尝试「扩展」，直到无法扩展为止，
+此时的回文串长度即为此「回文中心」下的最长回文串长度。我们对所有的长度求出最大值，即可得到最终的答案。
+*/
+
+/*************************************************************/
+//方法二：动态规划
+//https://leetcode-cn.com/problems/longest-palindromic-substring/solution/zui-chang-hui-wen-zi-chuan-by-leetcode-solution/
