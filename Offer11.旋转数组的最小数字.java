b47849796df5491528519726edc69c0b11588b42
kinwgze <wz297443228@outlook.com>
2020-08-26 16:18:22 +0800

    first code

---
+++ Offer11.旋转数组的最小数字.java
@@ -0,0 +1,22 @@
+//二分法
+class Solution {
+    public int minArray(int[] numbers) {
+        int low = 0;
+        int high = numbers.length - 1;
+        while (low < high) {
+            int pivot = low + (high - low) / 2;
+            if (numbers[pivot] < numbers[high]) {
+                high = pivot;
+            } else if (numbers[pivot] > numbers[high]) {
+                low = pivot + 1;
+            } else {
+                high -= 1;
+            }
+        }
+        return numbers[low];
+    }
+}
+
+
+
+//可能是测试用例的关系，用暴力法反而会时间更短一些
