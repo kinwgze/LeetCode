de3fccd768ae89f0837e2725268d8eacbde8598d
kinwgze <wz297443228@outlook.com>
2020-09-01 14:24:04 +0800

    hard

---
+++ Offer19.正则表达式匹配.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int n = s.length();
+        int m = p.length();
+        boolean[][] f = new boolean[n + 1][m + 1];
+
+        for (int i = 0; i <= n; i ++){
+            for (int j = 0; j <= m; j ++) {
+                if (j == 0) {
+                    f[i][j] = i == 0;
+                } else {
+                    if (p.charAt(j - 1) != '*') {
+                        if (i > 0 && (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '.')) {
+                            f[i][j] = f[i - 1][j - 1];
+                        }
+                    } else {
+                        if (j >= 2) {
+                            f[i][j] |= f[i][j - 2];
+                        }
+
+                        if (i >= 1 && j >= 2 && (s.charAt(i - 1) == p.charAt(j - 2) || p.charAt(j - 2) == '.')) {
+                            f[i][j] |= f[i - 1][j];
+                        }
+                    }
+                }
+            }
+        }
+        return f[n][m];
+    }
+}
